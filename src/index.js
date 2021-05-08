15b4413681d68695ae9d85c8d222c02cf8fa316e
watupzack <tvletsplay412@gmail.com>
2021-05-08 22:54:32 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>Water is boiling</p>;
+    } else {
+        return <p>Water is NOT boiling</p>
+    }
+}
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+        this.state = {temperature: '', scale: 'c'};
+    }
+    handleCelsiusChange(temperature) {
+        this.setState({scale: 'c', temperature});
+    }
+    handleFahrenheitChange(temperature) {
+        this.setState({scale: 'f', temperature});
+    }
+    render() {
+        const scale = this.state.scale;
+        const temperature = this.state.temperature;
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+        return (
+            <div>
+                <TemperatureInput 
+                    scale="c"
+                    temperature={celsius}
+                    onTemperatureChange={this.handleCelsiusChange}
+                />
+                <TemperatureInput 
+                    scale="f"
+                    temperature={fahrenheit}
+                    onTemperatureChange={this.handleFahrenheitChange}
+                />
+                <BoilingVerdict celsius={parseFloat(celsius)} />
+            </div>
+        );
+    }
+}
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+}
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+        // this.setState({temperature: event.target.value});
+        this.props.onTemperatureChange(event.target.value)
+    }
+    render() {
+        const temperature = this.props.temperature;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input 
+                    value={temperature}
+                    onChange={this.handleChange}
+                />
+            </fieldset>
+        );
+    }
+}
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+function tryConvert(temperature, convertFunction) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convertFunction(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Calculator />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
